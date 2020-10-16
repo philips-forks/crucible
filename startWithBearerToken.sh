33338dbef898730010859013243f8761dbe2a897
Gert Jan Kamstra <gert.jan.kamstra@philips.com>
2020-10-16 15:31:22 +0200

    Complete set token script

--- startWithBearerToken.sh
+++ startWithBearerToken.sh
@@ -4,12 +4,19 @@ echo "It will then build the docker image and start the containers."
 
 read -n 1 -s -r -p "Press any key to continue"
 
-tokenstring = "##token##"
+tokenstring="##token##"
 read -p "Enter the token: " token
 
 git stash
 
-sed -i "s/$tokenstring/$token/" app/controllers/smarts_controller.rb
-sed -i "s/$tokenstring/$token/"app/models/server.rb
-sed -i "s/$tokenstring/$token/" app/models/test_result.rb
-sed -i "s/$tokenstring/$token/" app/models/test_run.rb
+sed -i .bak "s/$tokenstring/$token/" app/controllers/smarts_controller.rb
+rm app/controllers/smarts_controller.rb.bak
+sed -i .bak "s/$tokenstring/$token/" app/models/server.rb
+rm app/models/server.rb.bak
+sed -i .bak "s/$tokenstring/$token/" app/models/test_result.rb
+rm app/models/test_result.rb.bak
+sed -i .bak "s/$tokenstring/$token/" app/models/test_run.rb
+rm app/models/test_run.rb.bak
+
+docker-compose build
+docker-compose up
