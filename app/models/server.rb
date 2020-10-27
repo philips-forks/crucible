e103197cf2db80be9568fcda45eedf6fb06945fc
Gert Jan Kamstra <gert.jan.kamstra@philips.com>
2020-10-27 15:35:10 +0100

    Add FHIR 3.0.2 support

--- app/models/server.rb
+++ app/models/server.rb
@@ -312,7 +312,7 @@ class Server
 
           if ['1.0.2', '1.0.1', '1.0.0', '0.5.0', '0.4.0', '0.40'].include? version_abbreviated
             self.fhir_sequence = 'DSTU2'
-          elsif ['3.0.0', '3.0.1', '1.8.0', '1.6.0', '1.4.0', '1.2.0', '1.1.0'].include? version_abbreviated
+          elsif ['3.0.0', '3.0.1','3.0.2' '1.8.0', '1.6.0', '1.4.0', '1.2.0', '1.1.0'].include? version_abbreviated
             self.fhir_sequence = 'STU3'
           else # Set to most recent sequence and version if not STU3 or DSTU2
             self.fhir_sequence = 'R4'
